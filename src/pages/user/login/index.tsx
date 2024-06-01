00076e7dd037fddbf88d00c6fe78fc917bb99d86
xuangegit <18321683010@163.com>
2024-06-01 17:17:57 +0800

    feat:登录

--- src/pages/user/login/index.tsx
+++ src/pages/user/login/index.tsx
@@ -167,7 +167,7 @@ const Login: React.FC = () => {
             maxWidth: '75vw',
           }}
           logo={<img alt="logo" src="/logo.svg" />}
-          title="Ant Design"
+          title="物联网平台"
           subTitle={intl.formatMessage({ id: 'pages.layouts.userLayout.title' })}
           initialValues={{
             autoLogin: true,
