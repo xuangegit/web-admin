cd0f13b7e76c0f64dd276075254243cf820aca97
xuanqianhui <18324683010.com>
2024-06-07 20:57:51 +0800

    feat:确认订单

---
+++ src/pages/mall/confirmOrder/mock.ts
@@ -0,0 +1,28 @@
+export const addressOptions = [
+    {name:'小哥哥',phone:'1234567890',address:'上海市浦东新区，中心校区23号楼1501室',id:1},
+    {name:'张俊',phone:'1234567890',address:'上海市浦东新区，中心校区23号楼1501室',id:2},
+    {name:'小姐姐',phone:'1234567890',address:'上海市浦东新区，中心校区23号楼1501室',id:3},
+    {name:'花美男',phone:'1234567890',address:'上海市松江区莘砖公路576号双子楼B座19层',id:4},
+]
+export const tableData =[{
+    name:'商品名称1',
+    price:100,
+    id:1,
+    quietPeriod:3,
+    totalPeriod:6,
+    carPrice:5,
+    num:16,
+    totalMoney:500
+
+},
+{
+    name:'商品名称2',
+    price:100,
+    id:2,
+    quietPeriod:3,
+    totalPeriod:6,
+    carPrice:5,
+    num:25,
+    totalMoney:500
+
+}]
