5ded40af75f07801983450cb7485d841a9c39b21
xuangegit <18321683010@163.com>
2024-06-09 15:15:22 +0800

    feat:订单管理

--- src/pages/mall/order/index.tsx
+++ src/pages/mall/order/index.tsx
@@ -1,5 +1,5 @@
 import { PageContainer, ProList } from '@ant-design/pro-components';
-import { Col, Flex, Row } from 'antd';
+import { Card, Col, Flex, Row } from 'antd';
 import dayjs from 'dayjs';
 import cmccImg from '../shoppingCar/images/cmcc.png';
 import { data } from '../shoppingCar/mock';
@@ -8,12 +8,23 @@ import { defaultData } from './mock';
 export default () => {
   return (
     <PageContainer>
-      <div>
+      <Card style={{minWidth:950}} >
+        <Row align={'middle'} wrap={false} className={styles.header} >
+          <Col flex="0 0 248px">商品</Col>
+          <Col flex="0 0 100px">套餐价格</Col>
+          <Col flex="0 0 60px">总周期</Col>
+          <Col flex="0 0 60px">沉默期</Col>
+          <Col flex="0 0 80px">单张卡费</Col>
+          <Col flex="0 0 80px">卡片数量</Col>
+          <Col flex="1 0 80px">订单价格</Col>
+          <Col flex="1 0 80px">订单状态</Col>
+          <Col flex="1 0 80px">操作</Col>
+        </Row>
         <ProList
           header={'34234'}
           dataSource={defaultData}
           rowKey={'id'}
-          // ghost
+          ghost
           renderItem={(item) => {
             console.log('item', item);
             return (
@@ -31,25 +42,35 @@ export default () => {
                     </Flex>
                   </Col>
                 </Row>
-                <Row>
-                  <Col flex={'0 0 256px'} style={{ borderRight: '1px solid #e5e5e5' }}>
+                <Row align={'middle'} wrap={false} className={styles.row}>
+                  <Col flex={'0 0 630px'} style={{ borderRight: '1px solid #e5e5e5' }}>
                     {data.map((item: any) => (
-                      <a key={item.id} className={styles.titleWrapper}>
-                        <Flex align="center" key={item.id} gap={10} >
-                          <img src={cmccImg} className={styles.shopImg} />
-                          <div>{item?.priceOffer?.name}</div>
-                        </Flex>
-                      </a>
+                      <Row key={item.id} align="middle" className={styles.info}>
+                        <Col flex="0 0 240px">
+                          <a key={item.id} className={styles.titleWrapper}>
+                            <Flex align="center" key={item.id} gap={10}>
+                              <img src={cmccImg} className={styles.shopImg} />
+                              <div>{item?.priceOffer?.name}</div>
+                            </Flex>
+                          </a>
+                        </Col>
+                        <Col flex="0 0 100px">￥352</Col>
+                        <Col flex="0 0 60px">2份</Col>
+                        <Col flex="0 0 60px">2月</Col>
+                        <Col flex="0 0 80px">￥352</Col>
+                        <Col flex="0 0 80px">3张</Col>
+                      </Row>
                     ))}
                   </Col>
-                  <Col>
-                  </Col>
+                  <Col flex="1 0 80px">￥3620</Col>
+                  <Col flex="1 0 80px">已完成</Col>
+                  <Col flex={'1 0 80px'}><a>详情</a></Col>
                 </Row>
               </div>
             );
           }}
         ></ProList>
-      </div>
+      </Card>
     </PageContainer>
   );
 };
